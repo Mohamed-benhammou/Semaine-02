cca2573421594acc01ff9e753da7012273d751f3
Mohamed <mohamed@siriusschool.be>
2020-06-17 12:00:14 +0200

    exercices JS/variable: 1.1 jusqu'a 1.4

---
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -0,0 +1,15 @@
+const helloWorld = 'Hello World';
+console.log(helloWorld);
+/* exercice 1.1 */
+const bonjour = `Bonjour je m'appelle`;
+const firstName = 'Mohamed';
+console.log(`${bonjour} ${firstName}`);
+/* exercice 1.2 */
+const iHave = "J'ai";
+let age = 29;
+const year = 'ans';
+console.log(`${iHave} ${age} ${year}`);
+/* exercice 1.3 */
+let siriusGroupeArray = ['Jo', 'Mo', 'Lea', 'Adam', 'Noé', 'Ali', 'Mike'];
+console.log(siriusGroupeArray[6]);
+/* exercice 1.4 */
