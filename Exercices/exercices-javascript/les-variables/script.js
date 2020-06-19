eeb3e57c12eda62837470e995cc086643b288bf9
Mohamed <mohamed@siriusschool.be>
2020-06-19 19:54:25 +0200

    Exercice fini - Js: variables

--- Exercices/exercices-javascript/les-variables/script.js
+++ Exercices/exercices-javascript/les-variables/script.js
@@ -10,6 +10,17 @@ let age = 29;
 const year = 'ans';
 console.log(`${iHave} ${age} ${year}`);
 /* exercice 1.3 */
-let siriusGroupeArray = ['Jo', 'Mo', 'Lea', 'Adam', 'Noé', 'Ali', 'Mike'];
-console.log(siriusGroupeArray[6]);
+let siriusGroupeArray = ['Jo', 'Mohamed', 'Lea', 'Adam', 'Noé', 'Ali', 'Mike'];
+/*console.log(siriusGroupeArray[6]);*/
 /* exercice 1.4 */
+console.log(siriusGroupeArray[1]);
+/* exercice 1.5 */
+let student = {
+  firstName: 'Mohamed',
+  name: 'Benhammou',
+  age: 28,
+};
+/*console.log(student);*/
+/* exercice 1.6 */
+console.log(student.firstName);
+/* exercice 1.7 */
